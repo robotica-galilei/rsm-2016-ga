38757f29d4e9256bcf422bb5116b8b077f0d2dda
Francesco Ballerin <ballerin@arch-N5110.lsgalilei.org>
2016-10-20 18:05:33 +0200

    New motion planning file

---
+++ algorithms/motion_planning.py
@@ -0,0 +1,7 @@
+def dijkstra(start, end ,mat):
+    '''
+    This function gets the starting cell, the final cell and the matrix.
+    It returns the distance between the starting cell and the final cell.
+    '''
+    #Still to implement
+    return -1
